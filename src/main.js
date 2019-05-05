ba97193b22f3aa527d1f35fe850001691c12015e
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-05-05 20:21:31 +0200

    SDK external dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import { Wallet } from '@aeternity/aepp-sdk';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+(async () => {
+  const client = await Wallet({
+    url: 'https://sdk-testnet.aepps.com',
+    internalUrl: 'https://sdk-testnet.aepps.com',
+    compilerUrl: 'https://compiler.aepps.com',
+  });
+
+  console.log('client', client);
+  console.log('client height', await client.height());
+})();

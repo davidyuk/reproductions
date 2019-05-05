dd16c3e830fdd7d3901d781a9c6ab2eec41c81f3
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-05-05 17:39:57 +0200

    SDK is not working on iOS 9.3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import { Wallet } from '@aeternity/aepp-sdk/es';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+(async () => {
+  const client = await Wallet({
+    url: 'https://sdk-testnet.aepps.com',
+    internalUrl: 'https://sdk-testnet.aepps.com',
+    compilerUrl: 'https://compiler.aepps.com',
+  });
+
+  console.log('client', client);
+})();

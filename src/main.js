3d7b68dde3851e0e2dba02515462ef3860b433e7
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-12-20 06:19:16 +0100

    Fetch using axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -10,5 +11,5 @@ new Vue({
 const URL = 'https://api.coingecko.com/api/v3/simple/price?ids=aeternity&vs_currencies=usd';
 
 (async () => {
-  console.log(await (await fetch(URL)).json())
+  console.log((await axios.get(URL)).data)
 })()

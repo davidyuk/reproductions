c8bf0247cae01918311d494e0d103406288c6097
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-12-20 05:59:37 +0100

    Add plain fetch call

--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+const URL = 'https://api.coingecko.com/api/v3/simple/price?ids=aeternity&vs_currencies=usd';
+
+(async () => {
+  console.log(await (await fetch(URL)).json())
+})()

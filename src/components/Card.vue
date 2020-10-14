94cfd0a4eca152bcaf72c7dd2950fa763a0cbfd0
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-10-14 18:17:31 +0300

    Example of showing Card on hover with delay using css

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    Card content
+  </div>
+</template>
+
+<style scoped>
+.card {
+  position: absolute;
+  padding: 10px;
+  background: #ccc;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <button
+        @mouseover="hover = true"
+        @mouseleave="hover = false"
+    >
+      hover: {{ hover }}
+    </button>
+    <Transition name="fade">
+      <Card v-if="hover" />
+    </Transition>
+  </div>
+</template>
+
+<script>
+import Card from './Card';
+
+export default {
+  components: { Card },
+  data: () => ({ hover: false }),
+}
+</script>
+
+<style scoped>
+.header .card.fade-enter-active, .header .card.fade-leave-active {
+  transition: opacity .3s;
+}
+
+.header .card.fade-enter-to, .header .card.fade-leave-to {
+  transition-delay: .5s;
+}
+
+.header .card.fade-enter, .header .card.fade-leave-to {
+  opacity: 0;
+}
+</style>

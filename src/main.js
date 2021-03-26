2c3f7ad5ca95449e98b8223f73489f7d88a95171
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-03-26 21:28:46 +0300

    SDK typescript support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
-import 'es6-shim'
-import 'weakmap-polyfill'
+
+// to enable chrome 39 (Android 5.1)
+import 'core-js/features/symbol'
+import 'core-js/features/set'
+
+// to enable chrome 30 (Android 4.4)
+import 'core-js/features/weak-map'
+import 'core-js/features/map'
+import 'core-js/features/number/is-safe-integer'
+
 import { Universal, Node, MemoryAccount } from '@aeternity/aepp-sdk/es';
 import App from './App.vue'
 
@@ -11,12 +19,16 @@ new Vue({
 }).$mount('#app');
 
 (async () => {
+  console.log('inside');
+  const node = await Node({
+    url: 'https://sdk-testnet.aepps.com',
+  });
+  console.log('node', node);
+
   const sdk = await Universal({
     nodes: [{
       name: 'test-net',
-      instance: await Node({
-        url: 'https://sdk-testnet.aepps.com',
-      }),
+      instance: node,
     }],
     compilerUrl: 'https://compiler.aepps.com',
     accounts: [MemoryAccount({ keypair: {
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    transpileDependencies: ['@aeternity/aepp-sdk'],
-};

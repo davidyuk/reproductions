7aebcf2626af43086953ff11e9c03082c6c2305c
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-11-19 19:12:20 +0300

    Calldata is not working in app build with @vue/cli@4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import { Encoder } from '@aeternity/aepp-calldata'
 import App from './App.vue'
+import ContractAci from './assets/ContractAci.json'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+const encoder = new Encoder(ContractAci)
+console.log('Encode', encoder.encode('Test', 'init', [])) // expected 'cb_KxFE1kQfP4oEp9E='

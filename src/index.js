9dbdaff1bd21c3c9a4b71c7bf0032322f3355ff7
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-12-09 20:46:01 +0300

    Add calldata

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Encoder } from '@aeternity/aepp-calldata';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +16,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+console.log(Encoder);

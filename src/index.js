4a1f33add8990e85aa7534a05822334c33b3d6c6
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-02-09 18:38:27 +0300

    create-react-app@4 with uuid reproduction

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './test.mjs';
 
 ReactDOM.render(
   <React.StrictMode>
